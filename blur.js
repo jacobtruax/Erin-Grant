8d708881460dd8332f64b46a53d39f32ede94ca0
Jacob Truax <jtruax303@gmail.com>
2019-10-14 00:44:07 -0400

    Commit

--- blur.js
+++ blur.js
@@ -1,32 +1,30 @@
-const sections = document.querySelectorAll("section")
-const mainTag = document.querySelector("main")
-const divTag = document.querySelector("div.Loop")
-
-const addMovement = function() {
-  const topViewport = divTag.offsetTop
-  const midViewport = topViewport + (divTag.offsetHeight / 2)
-
-  sections.forEach(section => {
-    const topSection = section.offsetTop
-
-    const midSection = topSection + (section.offsetHeight / 2)
-    // console.log(midSection)
-
-    const distanceToSection = midViewport - midSection
-    console.log(distanceToSection)
-
-    const image = section.querySelector("img")
-
-    image.style.transform = `rotate(${distanceToSection}deg)`
-  })
-}
-
-addMovement()
-
-mainTag.addEventListener("scroll", function() {
-  addMovement()
-})
-
-window.addEventListener("resize", function() {
-  addMovement()
-})
+// const sections = document.querySelectorAll("section")
+// const divTag = document.querySelector("div.Loop")
+// const mainTag = document.querySelector("main")
+//
+// const addMovement = function() {
+//   const topViewport = window.pageYOffset
+//   const midViewport = topViewport + (window.innerHeight / 2)
+//
+//   sections.forEach(section => {
+//     const topSection = section.offsetTop
+//     const midSection = topSection + (section.offsetHeight / 2)
+//
+//     const distanceToSection = (midViewport - midSection)
+//     console.log(distanceToSection)
+//
+//     const image = section.querySelector("img")
+//
+//     image.style.transform = `rotate(${distanceToSection}deg)`
+//   })
+// }
+//
+// addMovement()
+//
+// document.addEventListener("scroll", function() {
+//   addMovement()
+// })
+//
+// window.addEventListener("resize", function() {
+//   addMovement()
+// })
--- main.js
+++ main.js
@@ -1,18 +1,69 @@
+const sections = document.querySelectorAll("section")
+const divTag = document.querySelector("div.Loop")
+const mainTag = document.querySelector("main")
+
 var doc = window.document,
-  context = doc.querySelector('.js-loop'),
-  clones = context.querySelectorAll('.is-clone'),
+  clones = divTag.querySelectorAll('.is-clone'),
   disableScroll = false,
   scrollHeight = 0,
   scrollPos = 0,
   clonesHeight = 0,
   i = 0;
 
+const addMovement = function() {
+  const topViewport = divTag.scrollTop
+  const midViewport = topViewport + (divTag.offsetHeight / 2)
+
+  sections.forEach(section => {
+    const topSection = section.offsetTop
+    const midSection = topSection + (section.offsetHeight / 2)
+
+    const distanceToSection = midViewport - midSection
+    // const skewToSection = (midViewport - midSection) / 75
+    // const skewToSection2 = (midViewport - midSection) / 100
+
+    const images = section.querySelectorAll("img")
+    const image2 = section.querySelector(".blur")
+
+    let rotation = distanceToSection / 100
+    let skew = -1 * distanceToSection / 50
+    let opacity = distanceToSection / 800
+    let top = (distanceToSection / 20) + 50
+    let left = (distanceToSection / 10) + 375
+    // let contentDist = -1 * distanceToSection / 2
+
+    images.forEach((image, index) => {
+      if(index % 2 == 1){
+        image.style.opacity = `${opacity}`
+        image.style.transform = `rotate(${rotation}deg)`
+        image.style.transform = `skewX(${skew}deg)`
+        image.style.bottom = `${top}px`
+        image.style.left = `${left}px`
+      }
+
+      if(index % 2 == 0){
+        image.style.transform = `rotate(${rotation}deg)`
+        image.style.transform = `skewX(${skew}deg)`
+      }
+    })
+
+    // image2.style.transform += `skewX(${skewToSection2}deg)`
+
+
+    // image.style.filter += `blur(${distanceToSection}px)`
+    // image.style.transform = `skewY(${-1 * skewToSection}deg)`
+    // image.style.top = `${contentDist}px`
+  })
+}
+
+addMovement()
+
 function getScrollPos () {
-  return (context.offsetTop || context.scrollTop) - (context.clientTop || 0);
+  return (divTag.offsetTop || divTag.scrollTop) - (divTag.clientTop || 0);
 }
 
 function setScrollPos (pos) {
-  context.scrollTop = pos;
+  divTag.scrollTop = pos;
 }
 
 function getClonesHeight () {
@@ -27,7 +78,7 @@ function getClonesHeight () {
 
 function reCalc () {
   scrollPos = getScrollPos();
-  scrollHeight = context.scrollHeight;
+  scrollHeight = divTag.scrollHeight;
   clonesHeight = getClonesHeight();
 
   if (scrollPos <= 0) {
@@ -43,10 +94,12 @@ function scrollUpdate () {
       // Scroll to the top when you’ve reached the bottom
       setScrollPos(1); // Scroll down 1 pixel to allow upwards scrolling
       disableScroll = true;
+      console.log("hello")
     } else if (scrollPos <= 0) {
       // Scroll to the bottom when you reach the top
       setScrollPos(scrollHeight - clonesHeight);
       disableScroll = true;
+      console.log("helloUp")
     }
   }
 
@@ -61,12 +114,14 @@ function scrollUpdate () {
 function init () {
   reCalc();
 
-  context.addEventListener('scroll', function () {
+  divTag.addEventListener('scroll', function () {
     window.requestAnimationFrame(scrollUpdate);
+    addMovement()
   }, false);
 
   window.addEventListener('resize', function () {
     window.requestAnimationFrame(reCalc);
+    addMovement()
   }, false);
 }
 
