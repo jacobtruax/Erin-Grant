3ee3dd6ae72b016266ee66ae7351fc8951ce97b8
Jacob Truax <jtruax303@gmail.com>
2019-10-17 04:21:40 -0400

    coommmmmmmmmmit

--- main.js
+++ main.js
@@ -344,7 +344,7 @@ var tlLoad = new TimelineMax({delay: .25, onStart:scrollOne})
 TweenMax.set("#real", {autoAlpha: 0, visibility: "hidden"})
 TweenMax.set("#circle", {visibility: "hidden"})
 
-tlLoad.fromTo("#real", 2.2, {y:10}, {visibility: "visible", autoAlpha:1, y: -30, ease:Power4.easeOut})
+tlLoad.fromTo("#real", 2.2, {visibility: "hidden", y:10}, {visibility: "visible", autoAlpha:1, y: -30, ease:Power4.easeOut})
 .to("#circle", 0.1, {visibility: "visible"}, 0)
 .fromTo("#infoBlock", 1, {x:100}, {autoAlpha:1, x: 0, ease:Power4.easeOut}, 1)
 //
